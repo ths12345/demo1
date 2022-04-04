69556e92162b3e1e39f3fe624c671807ef809cbf
ths <1977477531@qq.com>
2022-04-04 14:51:23 +0800

    文件HelloWorld.java的第1次提交

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hehe");
+	 System.out.println("HOT-FIX");
+        System.out.println("master");
+
+    }
+}
